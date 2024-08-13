c21a2b70885e35b1c4df5ad80b283c29f5b57e44
notYoosung <notYoosung4901@gmail.com>
2024-08-13 15:35:43 -0500

    farming

--- mc_mcl_conv.lua
+++ mc_mcl_conv.lua
@@ -1,7 +1,14 @@
-local mc = io.open("./mc_list_blocks.txt", "r")
 local mcl = io.open("./mcl_list.txt", "r")
 local matched = io.open("./matched.txt", "w")
 local unmatched = io.open("./unmatched.txt", "w")
+local function open_mc_list(name)
+    return io.open("./mc_list_" .. name .. ".txt", "r")
+end
+local mc_lists = {
+    open_mc_list("blocks"),
+    open_mc_list("mob_effect"),
+    open_mc_list("item"),
+}
 
 -- https://stackoverflow.com/a/7615129
 local function split(inputstr, sep)
@@ -15,84 +22,105 @@ local function split(inputstr, sep)
     return t
 end
 
-if mc ~= nil and mcl ~= nil and matched ~= nil and unmatched ~= nil then
-    local mc_list = mc:read("*a")
-    -- o:write(i_read)
-    local patterns = {}
-    
-    for line in mcl:lines() do
-        line = line:gsub("\\.png", "")
-        local un_mcl_line = line:gsub("(mcl_%a-_)", "")
-        local un_default_line = line:gsub("(default_)", "")
-        local line_split = split(line, "_")
-        local un_mcl_line_split = split(line:gsub("(mcl_%a-_)", ""), "_")
-        patterns = {
-            {
-                match = line,
-                -- cond = true,
-                -- output = "",
-            },
-            {
-                match = (line_split[2] or "") .. "_" .. (line_split[1] or ""),
-                cond = #line_split == 2,
-                -- output = "",
-            },
-            {
-                match = un_mcl_line,
-                cond = not line:match("copper"),
-                -- output = "%1",
-            },
-            {
-                match = (un_mcl_line_split[2] or "") .. "_" .. (un_mcl_line_split[1] or ""),
-                cond = #un_mcl_line_split == 2,
-                -- output = "",
-            },
-            {
-                match = line:gsub("mcl_copper_", "copper_"),
-                -- cond = ,
-                -- output = "",
-            },
-            {
-                match = line:gsub("xpanes_top_glass_(.-)", function (s)
-                    return s .. "_stained_glass"
-                end)
-            },
-            {
-                match = un_default_line,
-                -- cond = ,
-                -- output = "%1",
-            },
-            -- {
-            --     match = "mcl_%a+_" .. line_split[2] .. "_" .. line_split[1],
-            --     cond = #line_split == 2,
-            --     -- output = "",
-            -- },
-        }
-        local found_match = false
-        for _, pattern in ipairs(patterns) do
-            local cond = pattern.cond == nil or pattern.cond == true
-            if not cond then
-                goto continue
+for _, raw_mc_list in ipairs(mc_lists) do
+    if raw_mc_list ~= nil and mcl ~= nil and matched ~= nil and unmatched ~= nil then
+        local mc_list = raw_mc_list:read("*a")
+        -- o:write(i_read)
+        local patterns = {}
+        
+        for line in mcl:lines() do
+            line = line:gsub("\\.png", "")
+            local un_mcl_line = line:gsub("(mcl_[a-z]-_)", "")
+            local un_default_line = line:gsub("(default_)", "")
+            local line_split = split(line, "_")
+            local un_mcl_line_split = split(line:gsub("(mcl_%a-_)", ""), "_")
+            local un_farming_line = line:gsub("(farming_)", "")
+            patterns = {
+                {
+                    match = line,
+                    -- cond = true,
+                    -- output = "",
+                },
+                {
+                    match = (line_split[2] or "") .. "_" .. (line_split[1] or ""),
+                    cond = #line_split == 2,
+                    -- output = "",
+                },
+                {
+                    match = un_mcl_line,
+                    cond = not line:match("copper"),
+                    -- output = "%1",
+                },
+                {
+                    match = (un_mcl_line_split[2] or "") .. "_" .. (un_mcl_line_split[1] or ""),
+                    cond = #un_mcl_line_split == 2,
+                    -- output = "",
+                },
+                {
+                    match = line:gsub("mcl_copper_", "copper_"),
+                    -- cond = ,
+                    -- output = "",
+                },
+                {
+                    match = line:gsub("xpanes_top_glass_(.-)", function (s)
+                        return s .. "_stained_glass"
+                    end)
+                },
+                {
+                    match = un_default_line,
+                    -- cond = ,
+                    -- output = "%1",
+                },
+                {
+                    match = line:gsub("mcl_potions_effect_", ""),
+                    -- cond = ,
+                    -- output = "",
+                },
+                {
+                    match = un_farming_line,
+                    -- cond = ,
+                    -- output = "%1",
+                },
+                {
+                    match = line:gsub("(mcl_boats_)", ""),
+                    -- cond = ,
+                    -- output = "%1",
+                },
+                -- {
+                --     match = "mcl_%a+_" .. line_split[2] .. "_" .. line_split[1],
+                --     cond = #line_split == 2,
+                --     -- output = "",
+                -- },
+            }
+            local found_match = false
+            if line:match("mcmeta") then goto skip end
+            for _, pattern in ipairs(patterns) do
+                local cond = pattern.cond == nil or pattern.cond == true
+                if not cond then
+                    goto continue
+                end
+                local match = mc_list:match("" .. pattern.match)
+                local output = match--pattern.output or match or ""
+                if match == nil then
+                    goto continue
+                end
+                matched:write("\"" .. line .. "\", \"" .. output .. "\"\n")
+                found_match = true
+                break
+                ::continue::
             end
-            local match = mc_list:match("" .. pattern.match)
-            local output = match--pattern.output or match or ""
-            if match == nil then
-                goto continue
+            if not found_match then
+                unmatched:write("\"" .. line .. "\"\n")
             end
-            matched:write("\"" .. line .. "\", \"" .. output .. "\"\n")
-            found_match = true
-            break
-            ::continue::
-        end
-        if not found_match then
-            unmatched:write("\"" .. line .. "\"\n")
-        end
-        -- o:write(line .. "\n")
+            ::skip::
+            -- o:write(line .. "\n")
 
+        end
+        
+        raw_mc_list:close()
     end
-    
-    mc:close()
-    mcl:close()
-    matched:close()
-    unmatched:close()
 end
+
+mcl:close()
+matched:close()
+unmatched:close()
